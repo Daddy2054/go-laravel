04d23370fe1060db05b36016fa9076b9c5929f0d
Jean <acalto2@gmail.com>
2023-11-22 20:34:05 +0100

    17. Trying out the Go render functionality

--- myapp/main.go
+++ myapp/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/daddy2054/celeritas"
+import (
+	"myapp/handlers"
+
+	"github.com/daddy2054/celeritas"
+)
 
 type application struct {
 	App *celeritas.Celeritas
+	Handlers *handlers.Handlers
 }
 
 func main() {
