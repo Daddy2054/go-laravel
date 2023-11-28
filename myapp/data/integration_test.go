935acd22cf4ed1299185d7db93d23886a45f2484
Jean <acalto2@gmail.com>
2023-11-28 18:20:11 +0100

    48. Creating tables in  test docker, running tests

--- myapp/data/integration_test.go
+++ myapp/data/integration_test.go
@@ -1,3 +1,6 @@
+//go:build integration
+
+// run tests with this command: go test . --tags integration --count=1
 package data
 
 import (
@@ -11,22 +14,21 @@ import (
 	"github.com/ory/dockertest/v3/docker"
 )
 
-
 var (
-	host = "localhost"
-	user = "postgres"
+	host     = "localhost"
+	user     = "postgres"
 	password = "secret"
-	dbName = "celeritas_test"
-	port = "5435"
-	dsn = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5"
+	dbName   = "celeritas_test"
+	port     = "5435"
+	dsn      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5"
 )
 
 var dummyUser = User{
 	FirstName: "Some",
-	LastName: "Guy",
-	Email: "me@here.com",
-	Active: 1,
-	Password: "password",
+	LastName:  "Guy",
+	Email:     "me@here.com",
+	Active:    1,
+	Password:  "password",
 }
 
 var models Models
@@ -34,8 +36,7 @@ var testDB *sql.DB
 var resource *dockertest.Resource
 var pool *dockertest.Pool
 
-
-func TestMain(m *testing.M){
+func TestMain(m *testing.M) {
 	os.Setenv("DATABASE_TYPE", "postgres")
 
 	p, err := dockertest.NewPool("")
@@ -47,7 +48,7 @@ func TestMain(m *testing.M){
 
 	opts := dockertest.RunOptions{
 		Repository: "postgres",
-		Tag: "13.4",
+		Tag:        "13.4",
 		Env: []string{
 			"POSTGRES_USER=" + user,
 			"POSTGRES_PASSWORD=" + password,
@@ -63,14 +64,16 @@ func TestMain(m *testing.M){
 
 	resource, err = pool.RunWithOptions(&opts)
 	if err != nil {
+		// if there is error, run
+		// sudo pkill docker && sudo systemctl restart docker
 		_ = pool.Purge(resource)
 		log.Fatalf("could not start resource: %s", err)
 	}
-
 	if err := pool.Retry(func() error {
 		var err error
 		testDB, err = sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbName))
 		if err != nil {
+			log.Println("Error: ", err)
 			return err
 		}
 		return testDB.Ping()
@@ -79,5 +82,157 @@ func TestMain(m *testing.M){
 		log.Fatalf("could not connect to docker: %s", err)
 	}
 
-	
+	err = createTables(testDB)
+	if err != nil {
+		log.Fatalf("error creating tables: %s", err)
+	}
+
+	models = New(testDB)
+
+	code := m.Run()
+
+	if err := pool.Purge(resource); err != nil {
+		log.Fatalf("could not purge resource: %s", err)
+	}
+
+	os.Exit(code)
+}
+
+func createTables(db *sql.DB) error {
+	stmt := `
+	CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+drop table if exists users cascade;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name character varying(255) NOT NULL,
+    last_name character varying(255) NOT NULL,
+    user_active integer NOT NULL DEFAULT 0,
+    email character varying(255) NOT NULL UNIQUE,
+    password character varying(60) NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now()
+);
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+drop table if exists remember_tokens;
+
+CREATE TABLE remember_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    remember_token character varying(100) NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now()
+);
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON remember_tokens
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+drop table if exists tokens;
+
+CREATE TABLE tokens (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    first_name character varying(255) NOT NULL,
+    email character varying(255) NOT NULL,
+    token character varying(255) NOT NULL,
+    token_hash bytea NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now(),
+    expiry timestamp without time zone NOT NULL
+);
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON tokens
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+	`
+
+	_, err := db.Exec(stmt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func TestUser_Table(t *testing.T) {
+	s := models.Users.Table()
+	if s != "users" {
+		t.Error("wrong table name returned: ", s)
+	}
+}
+
+func TestUser_Insert(t *testing.T) {
+	id, err := models.Users.Insert(dummyUser)
+	if err != nil {
+		t.Error("failed to insert user: ", err)
+	}
+
+	if id == 0 {
+		t.Error("0 returned as id after insert")
+	}
+}
+
+func TestUser_Get(t *testing.T) {
+	u, err := models.Users.Get(1)
+	if err != nil {
+		t.Error("failed to get user: ", err)
+	}
+
+	if u.ID == 0 {
+		t.Error("id of returned user is 0: ", err)
+	}
+}
+
+func TestUser_GetAll(t *testing.T) {
+	_, err := models.Users.GetAll()
+	if err != nil {
+		t.Error("failed to get user: ", err)
+	}
+}
+
+func TestUser_GetByEmail(t *testing.T) {
+	u, err := models.Users.GetByEmail("me@here.com")
+	if err != nil {
+		t.Error("failed to get user: ", err)
+	}
+
+	if u.ID == 0 {
+		t.Error("id of returned user is 0: ", err)
+	}
+}
+
+func TestUser_Update(t *testing.T) {
+	u, err := models.Users.Get(1)
+	if err != nil {
+		t.Error("failed to get user: ", err)
+	}
+
+	u.LastName = "Smith"
+	err = u.Update(*u)
+	if err != nil {
+		t.Error("failed to update user: ", err)
+	}
+
+	u, err = models.Users.Get(1)
+	if err != nil {
+		t.Error("failed to get user: ", err)
+	}
+
+	if u.LastName != "Smith" {
+		t.Error("last name not updated in database")
+	}
 }
