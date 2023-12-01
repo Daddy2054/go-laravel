5c7e37fb10c22ae2f089b3f01bd59605dddb4eaf
Jean <acalto2@gmail.com>
2023-12-01 13:45:13 +0100

    73. Trying out our model validation

--- myapp/routes.go
+++ myapp/routes.go
@@ -74,7 +74,8 @@ func (a *application) routes() *chi.Mux {
 		u.LastName = a.App.RandomString(10)
 
 		validator := a.App.Validator(nil)
-		validator.Check(len(u.LastName) > 20, "las_name", "Last name must be 20 characters or more")
+		u.LastName = ""
+		u.Validate(validator)
 
 		if !validator.Valid() {
 			fmt.Fprint(w, "failed validation")
