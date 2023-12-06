a8fd97d650cad74917cd267d4ac7006877f7d762
Jean <acalto2@gmail.com>
2023-12-06 21:11:21 +0100

    122. Removing the .git  and creating a .env file

--- celeritas/cmd/cli/new.go
+++ celeritas/cmd/cli/new.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -9,36 +10,51 @@ import (
 	"github.com/go-git/go-git/v5"
 )
 
-
 func doNew(appName string) {
 	appName = strings.ToLower(appName)
 
 	// sanitize the application name (convert url to single word)
 	if strings.Contains(appName, "/") {
 		exploded := strings.SplitAfter(appName, "/")
-		appName = exploded[(len(exploded) -1)]
+		appName = exploded[(len(exploded) - 1)]
 	}
 
 	log.Println("App name is", appName)
 
-		// git clone the skeleton application
-		color.Green("\tCloning repository...")
-		_, err := git.PlainClone("./"+appName, false, &git.CloneOptions{
-			URL: "https://github.com/Daddy2054/celeritas-app.git",
-			Progress: os.Stdout,
-			Depth: 1,
-		})
-		if err != nil {
-			exitGracefully(err)
-		}
+	// git clone the skeleton application
+	color.Green("\tCloning repository...")
+	_, err := git.PlainClone("./"+appName, false, &git.CloneOptions{
+		URL:      "https://github.com/Daddy2054/celeritas-app.git",
+		Progress: os.Stdout,
+		Depth:    1,
+	})
+	if err != nil {
+		exitGracefully(err)
+	}
 
 	// remove .git directory
-
+	err = os.RemoveAll(fmt.Sprintf("./%s/.git", appName))
+	if err != nil {
+		exitGracefully(err)
+	}
 	// create a ready to go .env file
+	color.Yellow("\tCreating .env file...")
+	data, err := templateFS.ReadFile("templates/env.txt")
+	if err != nil {
+		exitGracefully(err)
+	}
 
+	env := string(data)
+	env = strings.ReplaceAll(env, "${APP_NAME}", appName)
+	env = strings.ReplaceAll(env, "${KEY}", cel.RandomString(32))
+
+	err = copyDataToFile([]byte(env), fmt.Sprintf("./%s/.env", appName))
+	if err != nil {
+		exitGracefully(err)
+	}
 	// create a makefile
 
-	// update the go.mod file 
+	// update the go.mod file
 
 	// update existing .go files with correct name/imports
 
