f7101cafdf728716300084077be616d5e344f52b
Jean <acalto2@gmail.com>
2023-12-09 18:57:39 +0100

    25. Implementing the Put function for sFTP

--- celeritas/filesystems/sftpfilesystem/sftp.go
+++ celeritas/filesystems/sftpfilesystem/sftp.go
@@ -1,7 +1,16 @@
 package sftpfilesystem
 
-import "github.com/daddy2054/celeritas/filesystems"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path"
 
+	"github.com/daddy2054/celeritas/filesystems"
+	"github.com/pkg/sftp"
+	"golang.org/x/crypto/ssh"
+)
 
 type SFTP struct {
 	Host string
@@ -10,8 +19,56 @@ type SFTP struct {
 	Port string
 }
 
+func (s *SFTP) getCredentials() (*sftp.Client, error) {
+	addr := fmt.Sprintf("%s:%s", s.Host, s.Port)
+	config := &ssh.ClientConfig{
+		User: s.User,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(s.Pass),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	conn, err := ssh.Dial("tcp", addr, config)
+	if err != nil {
+		return nil, err
+	}
+	client, err := sftp.NewClient(conn)
+	if err != nil {
+		return nil, err
+	}
+	cwd, err := client.Getwd()
+	if err != nil {
+		return nil, err
+	}
+	log.Println("Current working directory:", cwd)
+
+	return client, nil
+
+}
 
 func (s *SFTP) Put(fileName, folder string) error {
+	client, err := s.getCredentials()
+	if err != nil {
+		return err
+	}
+	defer client.Close()
+
+	f, err := os.Open(fileName)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	f2, err := client.Create(path.Base(fileName))
+	if err != nil {
+		return err
+	}
+	defer f2.Close()
+
+	if _, err := io.Copy(f2, f); err != nil {
+		return err
+	}
+
 	return nil
 }
 
