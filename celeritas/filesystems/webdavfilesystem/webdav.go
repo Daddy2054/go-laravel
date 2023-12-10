fe96659cfbbcc2607466ad36039d77dfefda9c6f
Jean <acalto2@gmail.com>
2023-12-10 18:43:00 +0100

    35. Implementing the List function for WebDAV

--- celeritas/filesystems/webdavfilesystem/webdav.go
+++ celeritas/filesystems/webdavfilesystem/webdav.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strings"
 
 	"github.com/daddy2054/celeritas/filesystems"
 	"github.com/studio-b12/gowebdav"
 )
-
 
 type WebDAV struct {
 	Host string
@@ -37,8 +37,30 @@ func (w *WebDAV) Put(fileName, folder string) error {
 	return nil
 }
 
-func (s *WebDAV) List(prefix string) ([]filesystems.Listing, error) {
+// List lists files on the remote file system
+func (w *WebDAV) List(prefix string) ([]filesystems.Listing, error) {
 	var listing []filesystems.Listing
+	client := w.getCredentials()
+	files, err := client.ReadDir(prefix)
+	if err != nil {
+		return listing, err
+	}
+
+	for _, file := range files {
+		if !strings.HasPrefix(file.Name(), ".") {
+			b := float64(file.Size())
+			kb := b / 1024
+			mb := kb / 1024
+			current := filesystems.Listing{
+				LastModified: file.ModTime(),
+				Key:          file.Name(),
+				Size:         mb,
+				IsDir:        file.IsDir(),
+			}
+			listing = append(listing, current)
+		}
+	}
+
 	return listing, nil
 }
 
